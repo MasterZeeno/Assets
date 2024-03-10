24b1d683b62b2f69c9577437427dffc60ffe5488
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-11 00:26:56 +0800

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const header = document.querySelector(".header");
     const profile = document.querySelector(".profile");
-    const name = document.querySelector(".name");
     const name1 = document.querySelector(".name1");
     const name2 = document.querySelector(".name2");
 
@@ -20,6 +19,41 @@ document.addEventListener("DOMContentLoaded", event => {
         }
         name1.innerText = names[0];
         name2.innerText = names[1] || "";
+    }
+
+    function pauseOthers(currentVid) {
+        const videos = document.querySelectorAll("video");
+        videos.forEach(otherVideo => {
+            if (otherVideo !== currentVid && !otherVideo.paused) {
+                otherVideo.pause();
+                otherVideo.classList.add("notplaying");
+            }
+        });
+    }
+
+    function createVideoJs(video_source, image_source) {
+        const player = document.createElement("video");
+        player.setAttribute("class", "notplaying");
+        player.setAttribute("poster", image_source);
+        player.setAttribute("autoplay", "false");
+        player.setAttribute("preload", "metadata"); // Lazy load video metadata
+        // player.setAttribute("controls", ""); // Show default controls
+        const player_source = document.createElement("source");
+        player_source.setAttribute("src", video_source);
+        player_source.setAttribute("type", video_type);
+        player.append(player_source);
+        player.addEventListener("click", function () {
+            if (player.paused) {
+                player.play();
+                toggleFullScreen(player)
+                player.classList.remove("notplaying");
+                pauseOthers(player);
+            } else {
+                player.pause();
+                player.classList.add("notplaying");
+            }
+        });
+        return player;
     }
 
     const videoList = document.querySelector(".video-list");
@@ -40,56 +74,18 @@ document.addEventListener("DOMContentLoaded", event => {
         }
     }
 
-    function pauseOthers(currentVid) {
-        const videos = document.querySelectorAll("video");
-        videos.forEach(otherVideo => {
-            if (otherVideo !== currentVid && !otherVideo.paused) {
-                otherVideo.pause();
-                otherVideo.classList.add("notplaying");
-            }
-        });
-    }
-
-    function createVideoJs(video_source, image_source) {
-        const player = document.createElement("video");
-        player.setAttribute("class", "notplaying");
-        player.setAttribute("poster", image_source);
-        player.setAttribute("autoplay", "false");
-        // player.setAttribute("muted", "false");
-        player.setAttribute("name", "media");
-        // player.setAttribute("controls", false); // Hide default controls
-        const player_source = document.createElement("source");
-        player_source.setAttribute("src", video_source);
-        player_source.setAttribute("type", video_type);
-        player.append(player_source);
-        player.addEventListener("click", function () {
-            if (player.paused) {
-                player.play();
-                player.classList.remove("notplaying");
-                pauseOthers(player);
-                toggleFullScreen(player);
-                lastPlayedVideo = player;
-            } else {
-                player.pause();
-                player.classList.add("notplaying");
-            }
-        });
-        videoList.append(player);
-    }
-
-    url =
-        "https://script.google.com/macros/s/AKfycbx__TeLYl-rasvQ2msCnxNI7MpZB4BBp2Xmm-ZcTppgvRnrc4uQnCGWyUZlk5mppcD9/exec";
+    const url = "https://script.google.com/macros/s/AKfycbx__TeLYl-rasvQ2msCnxNI7MpZB4BBp2Xmm-ZcTppgvRnrc4uQnCGWyUZlk5mppcD9/exec";
 
     fetch(url)
         .then(response => response.json())
         .then(res => {
-            header.style.backgroundImage = "url('" + res.header + "')";
+            header.style.backgroundImage = `url('${res.header}')`;
             profile.setAttribute("src", res.profile);
             splitName(res.name);
-            // name.innerText = res.name;
             res.urls.forEach(e => {
                 if (e.video_url && e.image_url) {
-                    createVideoJs(e.video_url, e.image_url);
+                    const player = createVideoJs(e.video_url, e.image_url);
+                    videoList.append(player);
                 }
             });
         });
@@ -99,14 +95,13 @@ document.addEventListener("DOMContentLoaded", event => {
             pauseOthers(lastPlayedVideo);
             // Pan to the last played video player
             setTimeout(() => {
-                // Pan to the last played video player
                 const rect = lastPlayedVideo.getBoundingClientRect();
                 window.scrollTo({
-                    top: rect.top + window.scrollY - rect.height,
+                    top: rect.top + window.scrollY,
                     left: rect.left + window.scrollX,
                     behavior: "smooth"
                 });
-            }, 500); // Adjust delay time as needed
+            }, 500);
         }
     });
 });
