098f40038e371dc0257d7e8041ce79c076619ba3
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-11 07:17:00 +0800

    update index.js

--- index.js
+++ index.js
@@ -166,8 +166,8 @@ document.addEventListener("DOMContentLoaded", event => {
         // pauseOthers(lastPlayedVideo);
         if (!document.fullscreenElement && lastPlayedVideo) {
             // if (lastPlayedVideo.paused) {
-//                 lastPlayedVideo.play();
-//             }
+            //                 lastPlayedVideo.play();
+            //             }
             setTimeout(() => {
                 const rect = lastPlayedVideo.getBoundingClientRect();
                 window.scrollTo({
