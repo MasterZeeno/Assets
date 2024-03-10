572f2bb4c87b37c653a12a659706b016fc5c4957
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-11 02:24:49 +0800

    update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ document.addEventListener("DOMContentLoaded", event => {
                 for (let step = currPage; step <= lastPage; step++) {
                     const items = document.createElement("div");
                     items.innerText = step;
-                    if (String(step) == String(currPage)) {
+                    if (step) == String(currPage)) {
                         items.classList.add("current");
                     } else {
                         items.classList.add("notCurrent");
