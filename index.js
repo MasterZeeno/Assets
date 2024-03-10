d73fe5a407452b9337196cc61b7de9f937ae94fd
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-11 05:17:44 +0800

    update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", event => {
         player.append(player_source);
 
         player.addEventListener("click", function () {
-            if (!player.playing) {
+            if (player.paused) {
                 player.play();
                 // pauseOthers(player);
                 toggleFullScreen(player);
