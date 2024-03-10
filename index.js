2f50554f2159ee62c97045904c9937925f71690b
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-10 21:49:12 +0800

    update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", event => {
     fetch(url)
         .then(response => response.json())
         .then(res => {
-            header.setAttribute("background-image", `url(${res.header})`);
+            header.style.backgroundImage = `url(${res.header})`;
 
             profile.setAttribute("src", res.profile);
 
