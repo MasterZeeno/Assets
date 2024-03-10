d833e85aa56d830e8916f4a27e31e7aa215e8b2f
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-11 01:58:04 +0800

    update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ document.addEventListener("DOMContentLoaded", event => {
                 });
 
                 for (let step = currPage; step <= lastPage; step++) {
-                    document.createElement("div");
+                    document.createElement("div").innerText=step;
                 }
 
                 // Query for all videos after they are added to the DOM
