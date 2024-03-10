1e7f7a09d07160af295a2c17fb6e8e4f2b069cf2
Jay Ar Cimacio, RN, OHN, DM II <yawapisting7@gmail.com>
2024-03-11 01:14:32 +0800

    update index.js

--- index.js
+++ index.js
@@ -106,4 +106,18 @@ document.addEventListener("DOMContentLoaded", event => {
             }, 500);
         }
     });
+    const button = document.querySelector(".sticky");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // The video is visible, hide the button
+                button.style.display = "none";
+            } else {
+                // The video is not visible, show the button
+                button.style.display = "block";
+            }
+        });
+    });
+    observer.observe(lastPlayedVideo);
 });
